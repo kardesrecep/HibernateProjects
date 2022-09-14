35ecc4717f9d869d96b8540e5fa124034d615ff1
kardesrecep <recepkardes89@gmail.com>
2022-09-14 14:24:40 +0300

    14/09/2022

--- src/main/java/com/hb11/hql_criteriaapi/RunnerFetch11.java
+++ src/main/java/com/hb11/hql_criteriaapi/RunnerFetch11.java
@@ -2,6 +2,11 @@ package com.hb11.hql_criteriaapi;
 
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -54,15 +59,58 @@ public class RunnerFetch11 {
 //		int numOfRec2= query.executeUpdate();
 //		System.out.println("Effected Row Count:"+ numOfRec2);
 		
-		int matGrade=90;
+		//HQL query ile sum örneği-aggregation function
+//		int matGrade=90;
+//		
+//		String hqlQuery4="SELECT sum(s.mathGrade) FROM Student11 s where s.mathGrade>:grade";
+//		Query query= session.createQuery(hqlQuery4);
+//		query.setParameter("grade", matGrade);
+//		
+//	 	Long sumMathGrade=(Long) query.uniqueResult();
+//		System.out.println("Total math Grade:"+sumMathGrade);
 		
-		String hqlQuery4="SELECT sum(s.mathGrade) FROM Student11 s where s.mathGrade>:grade";
-		Query query= session.createQuery(hqlQuery4);
-		query.setParameter("grade", matGrade);
+		CriteriaBuilder cb= session.getCriteriaBuilder();
+	 	CriteriaQuery<Student11> criteriaQuery= cb.createQuery(Student11.class);
+		Root<Student11> root = criteriaQuery.from(Student11.class);
+	 	
+		//criteriaQuery.select(root);
+//		
+//		Query<Student11> query1 = session.createQuery(criteriaQuery);
+//		List<Student11> resultList= query1.getResultList();
+//	 	
+//		resultList.forEach(s->System.out.println(s));
 		
-	 	Long sumMathGrade=(Long) query.uniqueResult();
-		System.out.println("Total math Grade:"+sumMathGrade);
+//		criteriaQuery.select(root).where(cb.equal(root.get("name"),"Student Name 6"));
+//		Query<Student11> query2 = session.createQuery(criteriaQuery);
+//		List<Student11> resultList2= query2.getResultList();
+//		
+//		resultList2.forEach(System.out::println);
 		
+//		criteriaQuery.select(root).where(cb.greaterThan(root.get("mathGrade"),80));
+//		Query<Student11> query3 = session.createQuery(criteriaQuery);
+//		List<Student11> resultList3= query3.getResultList();
+//		resultList3.forEach(System.out::println);
+		
+//		criteriaQuery.select(root).where(cb.lessThan(root.get("mathGrade"),95));
+//		Query<Student11> query4 = session.createQuery(criteriaQuery);
+//		List<Student11> resultList4= query4.getResultList();
+//		resultList4.forEach(System.out::println);
+		
+		Long id=1L;
+		
+		Predicate predicateForName=cb.equal(root.get("id"), id);
+		Predicate predicateForMathGrade=cb.greaterThan(root.get("mathGrade"), 75);
+		
+		//Predicate predicateStd= cb.or(predicateForName,predicateForMathGrade);
+		Predicate predicateStd= cb.and(predicateForName,predicateForMathGrade);
+		
+		criteriaQuery.where(predicateStd);
+		Query<Student11> query5 = session.createQuery(criteriaQuery);
+		List<Student11> resultList5= query5.getResultList();
+		resultList5.forEach(System.out::println);
+		
+		
+	 	
 		tx.commit();
 		session.close();
 		sf.close();
